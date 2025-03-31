46cdefa1adaf1880fc148eceb8cf3bc76d596996
Khang <khang.nguyenthe@haravan.com>
2025-03-31 15:06:42 +0700

    fix

--- e2e-automation/utils/Login/fixture.js
+++ e2e-automation/utils/Login/fixture.js
@@ -4,6 +4,10 @@ import { LoginPage } from "../../pages/Login/loginPage";
 export const test = base.extend({
     loginPage: async ({ page }, use) => {
         const loginPage = new LoginPage(page);
+        await loginPage.goToLoginPage();
+        await loginPage.enterUsername("testuser");
+        await loginPage.enterPassword("password");
+        await loginPage.clickLoginButton();
         await use(loginPage);
     }
 });
